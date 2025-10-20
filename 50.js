5e660dce3b53a33f2c88bfccc5497433e3816031
laura <lcustodio@georgetowncollege.edu>
2025-10-20 09:18:35 -0500

    exerecices chapter 3

---
+++ 50.js
@@ -0,0 +1,28 @@
+//section 1
+/* 1.1. Declare a variable age and assign you age to it. Log its value.*/
+var age;
+age = 19;
+console.log(age);
+//> 19
+/* Declare a varible isStudent and assign a boolean value. Log its type*/
+var isStudent
+isStudent = true
+console.log(typeof isStudent)
+//>boolean
+/* Declare a string variable aboutMe that combines your name and age like "My name is Alex and I am 25 years old"*/
+let age = 19 
+let name = "Laura"
+const aboutMe = "My name is " + name  + " and I am "+ age +" years old"
+console.log(aboutMe)
+//> My name is Laura and I am 19 years old
+/* Create a constante variable PI and assign 3.14159 to it.*/
+const PI = 3.14159
+console.log(PI)
+//> 3.14159
+/* Log the result of dividing a number by zero*/
+let num = 6
+let div = 0
+console.log(num/div)
+//> infinity
+
+//section 2
